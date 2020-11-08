fc9ca67fc0e69c574b80d31f7bc7447188f87feb
andreasstove <andreasstove@outlook.dk>
2020-11-08 13:53:41 +0100

    PlutoContext

---
+++ Covid19_Tracking/Persistence/PlutoContext.cs
@@ -0,0 +1,29 @@
+﻿using Covid19_Tracking.Domain;
+using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Covid19_Tracking.Persistence
+{
+    class PlutoContext
+    {
+        public virtual DbSet<Citizen> Citizens { get; set; }
+        public virtual DbSet<CitizenLocation> CitizenLocations { get; set; }
+        public virtual DbSet<Location> Locations { get; set; }
+        public virtual DbSet<Municipality> Municipalities { get; set; }
+        public virtual DbSet<Nation> Nations { get; set; }
+        public virtual DbSet<TestCenter> TestCenters { get; set; }
+        public virtual DbSet<TestCenterManagement> TestCenterManagements { get; set; }
+        public virtual DbSet<TestDate> TestDates { get; set; }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+
+
+          
+        //}
+
+    }
+}
