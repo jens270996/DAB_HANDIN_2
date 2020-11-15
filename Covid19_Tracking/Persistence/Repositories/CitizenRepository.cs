cd29806cb189f45188af14b36028e040d83b691c
Jens Kristensen <jens-nk@hotmail.com>
2020-11-15 11:52:06 +0100

    Citizen Repos added

---
+++ Covid19_Tracking/Persistence/Repositories/CitizenRepository.cs
@@ -0,0 +1,19 @@
+﻿using Covid19_Tracking.Domain;
+using Covid19_Tracking.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Covid19_Tracking.Persistence.Repositories
+{
+    public class CitizenRepository : Repository<Citizen>, ICitizenRepository
+    {
+        public CitizenRepository(CovidContext context) : base(context)
+        {
+        }
+        public IEnumerable<Citizen> GetPossibleInfectedCitizens(Citizen infected)
+        {
+            throw (new NotImplementedException());
+        }
+    }
+}
